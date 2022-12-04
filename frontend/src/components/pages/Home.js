3b88a9a9c071ee42ff1e9afe45009c1288f3788d
sabakantaro <sbkryo290112@yahoo.co.jp>
2022-12-04 12:42:27 +0900

    add backend & frontend

---
+++ frontend/src/components/pages/Home.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import Header from "../pages/Header";
+
+const Home = () => {
+  return <Header/>;
+};
+
+export default Home;
---
+++ frontend/src/components/pages/SignIn.js
@@ -0,0 +1,135 @@
+/* eslint-disable jsx-a11y/alt-text */
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Cookies from "js-cookie";
+import { signIn } from "../../lib/api/gotoreAPI";
+import { getCurrentUser } from "../../lib/api/gotoreAPI";
+
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+// import FormControlLabel from "@mui/material/FormControlLabel";
+// import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+const SignIn = () => {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const theme = createTheme();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const body = {
+      email: email,
+      password: password,
+    };
+    try {
+      const res = await signIn(body);
+      console.log(res);
+
+      if (res.status === 200) {
+        Cookies.set("_access_token", res.headers["access-token"]);
+        Cookies.set("_client", res.headers["client"]);
+        Cookies.set("_uid", res.headers["uid"]);
+        getCurrentUser();
+        navigate("/");
+
+        console.log("Signed in successfully!");
+      } else {
+        console.log("failure");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component='main' maxWidth='xs'>
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component='h1' variant='h5'>
+            Sign in
+          </Typography>
+          <Box
+            component='form'
+            onSubmit={handleSubmit}
+            noValidate
+            sx={{ mt: 1 }}
+          >
+            <TextField
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              margin='normal'
+              required
+              fullWidth
+              id='email'
+              label='Email Address'
+              name='email'
+              autoComplete='email'
+              autoFocus
+            />
+            <TextField
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              margin='normal'
+              required
+              fullWidth
+              name='password'
+              label='Password'
+              type='password'
+              id='password'
+              autoComplete='current-password'
+            />
+            {/* <FormControlLabel
+              control={<Checkbox value='remember' color='primary' />}
+              label='Remember me'
+            /> */}
+            <Button
+              type='submit'
+              fullWidth
+              variant='contained'
+              sx={{ mt: 3, mb: 2 }}
+              onClick={handleSubmit}
+            >
+              Sign In
+            </Button>
+            <Grid container>
+              {/* <Grid item xs>
+                <Link href='#' variant='body2'>
+                  Forgot password?
+                </Link>
+              </Grid> */}
+              <Grid item>
+                <Link href='/signup' variant='body2'>
+                  Don't have an account? Sign Up
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
+  );
+};
+
+export default SignIn;
