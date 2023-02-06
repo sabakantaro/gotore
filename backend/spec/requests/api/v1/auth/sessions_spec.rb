5bbed9615783ff1ee2ed781dfd2d9872fff842d0
sabakantaro <sbkryo290112@yahoo.co.jp>
2023-02-06 20:57:22 +0900

    add request spec testing(WIP) #47

--- backend/spec/requests/api/v1/auth/sessions_spec.rb
+++ backend/spec/requests/api/v1/auth/sessions_spec.rb
@@ -1,7 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe "Api::V1::Auth::Sessions", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe 'SessionsAPI' do
+    it "get 404 without login" do
+      get '/api/v1/auth/sessions'
+
+      expect(response.status).to eq(401)
+    end
   end
 end
