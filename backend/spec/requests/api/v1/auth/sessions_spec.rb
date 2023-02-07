60889d899aa924ab275daaa939269ae01db194eb
sabakantaro <sbkryo290112@yahoo.co.jp>
2023-02-07 20:27:57 +0900

    add request spec testing #47

--- backend/spec/requests/api/v1/auth/sessions_spec.rb
+++ backend/spec/requests/api/v1/auth/sessions_spec.rb
@@ -2,7 +2,16 @@ require 'rails_helper'
 
 RSpec.describe "Api::V1::Auth::Sessions", type: :request do
   describe 'SessionsAPI' do
-    it "get 404 without login" do
+    it "get session user" do
+      FactoryBot.create(:user)
+      user = { email: "test@test.com", password: "password" }
+      auth_tokens = sign_in(user)
+      get '/api/v1/chat_rooms', headers: auth_tokens
+
+      expect(response.status).to eq(200)
+    end
+
+    it "get 401 without login" do
       get '/api/v1/auth/sessions'
 
       expect(response.status).to eq(401)
